e310ac41cce2a8a07dea1d1626531a5ee2062a0c
fragilehm <fragilehm2411@gmail.com>
2016-11-03 00:15:33 +0600

    Practice1

---
+++ PingPong/Assets/BallController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public float InputForceScale = 10.0f;
+    private Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    //Called for every Physics Engine Update step
+    void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(horizontalAxis, 0.0f, verticalAxis);
+        force *= InputForceScale;
+
+        rigidBody.AddForce(force);
+    }
+
+}
